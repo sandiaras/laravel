89c91ef72718194e854f8f54877716d085a70184
Sandi Aramayo <saramayo@serempre.com>
2022-03-14 21:51:44 -0400

    blog views

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller{
     public function index(){
-
+        return view('blog.index');
     }
 
     public function create(){
-
+        return view('blog.create');
     }
 
     public function show($entry){
-
+        return view('blog.show',['entry'=>$entry]);
     }
 }
